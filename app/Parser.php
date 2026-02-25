d204b4c7d9985f2bc466e00df86a9c1ac12e7773
Bradley McFadden <bmcfadden@laurentian.ca>
2026-02-25 15:51:29 -0500

    Initial naive solution

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,32 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $fh = fopen($inputPath, 'r');
+        if (!$fh) {
+            throw new Exception('input file not found');
+        }
+        $results = [];
+        $skipUrl = strlen('https://stitcher.io');
+        while ($line = fgets($fh)) {
+            $fields = explode(',', trim($line));
+            $path = substr($fields[0], $skipUrl);
+            $date = substr($fields[1], 0, 10); // 2026-01-24
+        
+            if (!array_key_exists($path, $results)) {
+                // $results[$path] needs to be ordered lexigraphically
+                $results[$path] = array();
+            }
+            if (!array_key_exists($date, $results[$path])) {
+                $results[$path][$date] = 1;
+            } else {
+                $results[$path][$date] += 1;
+            }
+        }
+        // change to by reference
+        foreach($results as &$days) {
+            ksort($days);
+        }
+        fclose($fh);
+        file_put_contents($outputPath, json_encode($results, JSON_PRETTY_PRINT));
     }
 }
